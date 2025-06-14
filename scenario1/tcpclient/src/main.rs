626d24367063432d99f00f408af82a65cbc14df2
DoK6n <ehrbs2018@gmail.com>
2025-06-14 13:18:12 +0900

    💡 unwrap, Result에 대한 설명 보강

--- scenario1/tcpclient/src/main.rs
+++ scenario1/tcpclient/src/main.rs
@@ -2,9 +2,14 @@ use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::str;
 
+/**
+    실제 프로덕션 코드에서는 unwrap가 아닌 match나 ? (try) 연산자를 사용해 적절한 에러 처리하거나 
+    [unwrap_or], [unwrap_or_else], 또는 [unwrap_or_default] 호출 해야 한다.
+    wnwrap은 성공한 경우 값을 반환하고 실패한 경우 패닉을 발생시킨다.
+    러스트에서 Result는 성공 또는 실패 가능성이 있는 연산의 결과를 나타내는 타입이다.
+    패닉은 프로그램이 예상치 못한 상황에서 종료되는 것을 의미한다.
+*/
 fn main() {
-    // 실제 프로덕션 코드에서는 unwrap가 아닌 match나 ? (try) 연산자를 사용해 적절한 에러 처리하거나 
-    // [unwrap_or], [unwrap_or_else], 또는 [unwrap_or_default] 호출 해야 한다.
 
     let mut stream = TcpStream::connect("localhost:3000").unwrap();
 
